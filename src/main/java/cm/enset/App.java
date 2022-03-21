d709223b4333a5c434fcad43b4678d34a7d1846c
Kaly Seven <kaly.salas.7@gmail.com>
2022-03-21 05:16:39 +0100

    Comments

--- src/main/java/cm/enset/App.java
+++ src/main/java/cm/enset/App.java
@@ -1,7 +1,11 @@
+/**
+ * comment
+ */
 package cm.enset;
 
 public class App{
     public static void main(String[] args){
+        //------------------------
         System.out.println("Hello !");
     }
 }
